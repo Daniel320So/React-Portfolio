4d681ed0e935158268d172fd3f44a69ac39eae4f
Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
2023-04-21 23:24:08 -0400

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,43 +5,46 @@ function Hero() {
   const tagLineRef = useRef(null);
   const h1Ref = useRef(null);
   const pRef = useRef(null);
+  let firstLoad = true;
 
   useEffect(() => {
+    if (firstLoad) {
+      // Handle text animations
+      let sec = 1500;
+      const tagLineEle = tagLineRef.current;
+      for (let i = 0; i < tagLineEle.children.length; i++) {
+        let ele = tagLineEle.children[i]
+        setTimeout(function () {
+          ele.style.display = "block";
+          ele.classList.add("animate-typing1");
+        }, sec)
+        sec = sec + 750
+        setTimeout(function () {
+          ele.style.width = "fit-content";
+          ele.style.animationPlayState = 'paused';
+          ele.classList.remove("animate-typing1");
+        }, sec)
+      }
 
-    // Handle text animations
-    let sec = 0;
-    const tagLineEle = tagLineRef.current;
-    for (let i = 0; i < tagLineEle.children.length; i++) {
-      let ele = tagLineEle.children[i]
+      const h1Ele = h1Ref.current;
       setTimeout(function () {
-        ele.style.display = "block";
-        ele.classList.add("animate-typing1");
+        h1Ele.style.display = "block";
+        h1Ele.classList.add("animate-typing2");
       }, sec)
-      sec = sec + 750
+      sec = sec + 1000;
       setTimeout(function () {
-        ele.style.width = "fit-content";
-        ele.style.animationPlayState = 'paused';
-        ele.classList.remove("animate-typing1");
+        h1Ele.style.width = "fit-content";
+        h1Ele.style.animationPlayState = 'paused';
+        h1Ele.classList.remove("animate-typing2");
+      }, sec)
+
+      const pEle = pRef.current;
+      setTimeout(function () {
+        pEle.style.display = "block";
+        pEle.classList.add("fade-in")
       }, sec)
     }
 
-    const h1Ele = h1Ref.current;
-    setTimeout(function () {
-      h1Ele.style.display = "block";
-      h1Ele.classList.add("animate-typing2");
-    }, sec)
-    sec = sec + 1000;
-    setTimeout(function () {
-      h1Ele.style.width = "fit-content";
-      h1Ele.style.animationPlayState = 'paused';
-      h1Ele.classList.remove("animate-typing2");
-    }, sec)
-
-    const pEle = pRef.current;
-    setTimeout(function () {
-      pEle.style.display = "block";
-      pEle.classList.add("fade-in")
-    }, sec)
   }, [])
 
   return (
